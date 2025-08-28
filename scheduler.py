52778655ca35307a6bfdfff5da5c7874442730ed
omole otieno <omolebill01@gmail.com>
2025-08-28 23:19:49 +0200

    Create data objects

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,12 @@
 from models import *
 from sqlalchemy.orm import Session
-from sqlalchemy import func, cast  
+from sqlalchemy import func, cast
 from database import SessionLocal
 import pandas as pd
 from datetime import timedelta, datetime, date
 from pprint import pprint
+from data_class import shiftSpecification, talentAvailability, assignment
+from utils import map_label_to_time
 
 db = SessionLocal()
 def load_talents(db: Session):
@@ -24,7 +26,7 @@ def load_talents(db: Session):
         Available_Day, Talent_Constraint.id == Available_Day.constraint_id, isouter=True).join(
             Requests, Talent.id == Requests.talent_id, isouter=True)
     df = pd.read_sql(query.statement, db.bind)
-    print(df)
+    #print(df)
     return df
 
 def talent_availability(start_date, end_date):
@@ -37,7 +39,7 @@ def talent_availability(start_date, end_date):
     constrained = talents.loc[(talents['constraint'].notna())].groupby(['talent_id', 'day']).agg({'shifts': lambda x: list(set(x)), 'role': 'first'}).reset_index().copy()
     constrained.loc[:,'date'] = constrained['day'].map(date_map).dt.date
     constrained = constrained[['talent_id', 'role', 'date', 'shifts']]
-    constrained = constrained.groupby('talent_id').agg({'talent_id': 'first', 'role': 'first', 'date': list, 'shifts': 'first'}) 
+    constrained = constrained.groupby('talent_id').agg({'talent_id': 'first', 'role': 'first', 'date': list, 'shifts': 'first'})
     #handle requests
     requests = talents.loc[(talents['req_date'].notna()) & (talents['status'] == 'approved')].groupby('talent_id')['req_date'].apply(lambda req: set(req)).reset_index()
     requests = requests.set_index('talent_id')['req_date'].to_dict()
@@ -50,8 +52,6 @@ def talent_availability(start_date, end_date):
     unconstrained.loc[:, 'shifts'] = [shifts] * len(unconstrained)
     unconstrained = unconstrained.drop_duplicates(subset=['talent_id'])
     all_talents = pd.concat([constrained, unconstrained], ignore_index=True).copy()
-    print(constrained.columns)
-    print(unconstrained.columns)
 
     #remove all the requested dates for every talent.
     for tid, dates in all_talents[['talent_id', 'date']].itertuples(index=None, name=None):
@@ -62,11 +62,56 @@ def talent_availability(start_date, end_date):
             all_talents = all_talents.drop(idx)
         else:
             all_talents.at[idx, 'date'] = filtered
-   
 
-    #print(all_talents)  
+    pprint(all_talents)
     return all_talents
-    
+
+
+def create_talent_objects(talents: pd.DataFrame, weeklyhours: float = 32) -> list[talentAvailability]:
+    """
+    Returns a list of talent objects from the talent dataframes
+
+    Args:
+        talents: dataframe with all available talents
+    Return:
+        list: talentAvailabilty
+    """
+    talent_list = talents.to_dict('records')
+    talent_object = []
+    for talent in talent_list:
+        for date in list(talent.get('date', [])):
+            window: dict = {}
+            window[date] = []
+            for shift in list(talent.get('shifts', [])):
+                shift_span = map_label_to_time(shift)
+                window[date].append(shift_span)
+            talent_object.append(talentAvailability(
+                talent_id=talent.get('talent_id'),
+                role=talent.get('role'),
+                window=window,
+                weeklyhours=weeklyhours
+            ))
+    return talent_object
+
+def create_shift_specification(shift_requirements: pd.DataFrame) -> list[shiftSpecification]:
+    """
+    Returns a list of objects from the shift_requirements dataframes
+
+    Args:
+        shift_requirements : dataframe of all shifts that need to be populated
+
+    Return:
+        list: shiftSpecification
+    """
+    shift_list = shift_requirements.to_dict('records')
+    shift_specification_object = []
+    for shift in shift_list:
+        shift.pop('day'); shift.pop('staffing'); shift.pop('shift')
+        shift_specification_object.append(shiftSpecification(**shift))
+
+    return shift_specification_object
+
+
 def load_shift_periods(db: Session):
     query = db.query(
         Shift_Period.id.label('period_id'),
@@ -83,7 +128,7 @@ def load_shift_periods(db: Session):
 
 def shift_requirements(start_date, end_date) :
     templates = load_shift_periods(db)
-    week = pd.date_range(start_date, end_date) 
+    week = pd.date_range(start_date, end_date)
     #here I built the dataframe from scratch then merged it with the existing templates based on matching staffing requirements
     week_df = pd.DataFrame({'date': [day.date() for day in week]})
     week_df.loc[:, 'day'] = [day.strftime("%A") for day in week]
@@ -91,7 +136,7 @@ def shift_requirements(start_date, end_date) :
     week_shifts = week_df.merge(templates[['staffing', 'shift', 'start', 'end', 'role', 'count']],on='staffing',how='left')
     return week_shifts
 
- 
+
 # use classes instead so that it is easier to scale
 # separate talent availability and shift_requirements
 def schedule_talents(start_date, end_date):
@@ -105,10 +150,10 @@ def morning_allowance():
     pass
 
 def is_eligible():
-    #this is a function to check the hard constraints that have to be enforced in order for a talent to be scheduled for a shift. 
+    #this is a function to check the hard constraints that have to be enforced in order for a talent to be scheduled for a shift.
     #This function is going to return true or false
     #The constraints here are:
-    # 1. A talent cannot work for more than 6 days straight 
+    # 1. A talent cannot work for more than 6 days straight
     # 2. There must be at least 11 hours between a talent's two shifts
     # 4. A talent cannot work for more than the hours on their contract (it can go up by 2-4 hours )
     pass
@@ -123,8 +168,13 @@ week = today + timedelta(days=6)
 
 #load_shift_periods(db)
 #load_talents(db)
-talent_availability(today, week)
-#data = shift_requirements(today, week)
+#talent= talent_availability(today, week)
+#objects = create_talent_objects(talent)
+#pprint(objects)
+
+data = shift_requirements(today, week)
+objects = create_shift_specification(data)
+print(objects)
 #pprint(data.to_dict('records'))
 #schedule_talents(today, week)
 
